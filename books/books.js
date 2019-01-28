92fca9b3e3d3123902a42c80e1c2ac3c5783344f
Nguyen Quang An <ngquangan@gmail.com>
2019-01-28 13:36:28 +0700

    Connected books service with mongodb

--- books/books.js
+++ books/books.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({path: '../.env'});
+
 const app = express();
+
+console.log(process.env.MONGO_URL);
 
 app.get('/', (req, res, next) => {
   res.send("This is book service main endpoint.");
+});
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true
+}, (err) => {
+  if(err) console.log(err);
+  console.log('Connected mongodb success');
 });
 
 const port = process.env.PORT || 5000;
