11cffdb1b6556a12b3e4515c698e2eeee14ca5fb
Nguyen Quang An <ngquangan@gmail.com>
2019-01-28 13:01:48 +0700

    Add route main book service

--- books/books.js
+++ books/books.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res, next) => {
+  res.send("This is book service main endpoint.");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server is running at port " + port);
+  console.log("Book service is running at port " + port);
 });
